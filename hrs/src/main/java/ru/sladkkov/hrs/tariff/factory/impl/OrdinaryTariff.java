fcc289e85d68b3d3da660ab4d6ee2599f4900f63
sladkkov <sladkkov@yandex.ru>
2023-04-25 13:02:18 +0400

    bug fix

--- hrs/src/main/java/ru/sladkkov/hrs/tariff/factory/impl/OrdinaryTariff.java
+++ hrs/src/main/java/ru/sladkkov/hrs/tariff/factory/impl/OrdinaryTariff.java
@@ -9,13 +9,12 @@ import java.time.Duration;
 
 public class OrdinaryTariff implements Tariffable {
 
-    private static final BigDecimal priceForMinute = BigDecimal.valueOf(1.5);
-    private static final BigDecimal ordinaryPrice = BigDecimal.valueOf(0.5);
-    private static final long fixedCountMinute = 100;
+    private static final BigDecimal PRICE_FOR_MINUTE = BigDecimal.valueOf(1.5);
+    private static final BigDecimal ORDINARY_PRICE = BigDecimal.valueOf(0.5);
+    private static final long FIXED_COUNT_MINUTE = 100;
 
     @Override
     public BigDecimal calculateCallPrice(CallDataRecordDto callDataRecordDto, Long sumTotalDuration) {
-
 
         if (callDataRecordDto.getTypeCall().getCode().equals("02")) {
 
@@ -27,20 +26,20 @@ public class OrdinaryTariff implements Tariffable {
 
         sumTotalDuration += totalDuration.toMinutes();
 
-        var val = fixedCountMinute - (sumTotalDuration - totalDuration.toMinutes());
-        if (sumTotalDuration > fixedCountMinute) {
+        var val = FIXED_COUNT_MINUTE - (sumTotalDuration - totalDuration.toMinutes());
+        if (sumTotalDuration > FIXED_COUNT_MINUTE) {
 
             if (val > 0) {
-                return ordinaryPrice
+                return ORDINARY_PRICE
                         .multiply(BigDecimal.valueOf(val))
-                        .add(priceForMinute.multiply(BigDecimal.valueOf(totalDuration.toMinutes() - val)));
+                        .add(PRICE_FOR_MINUTE.multiply(BigDecimal.valueOf(totalDuration.toMinutes() - val)));
 
             }
 
-            return priceForMinute.multiply(BigDecimal.valueOf(totalDuration.toMinutes()));
+            return PRICE_FOR_MINUTE.multiply(BigDecimal.valueOf(totalDuration.toMinutes()));
         }
 
-        return ordinaryPrice.multiply(BigDecimal.valueOf(totalDuration.toMinutes()));
+        return ORDINARY_PRICE.multiply(BigDecimal.valueOf(totalDuration.toMinutes()));
     }
 
 }
